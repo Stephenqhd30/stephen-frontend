2a42aa0c5610da8a2688853430d415a487f72f8b
stephenqiu <Popcornqhd@gmail.com>
2025-03-10 12:00:47 +0800

    refactor: 更新文件依赖 feat: 添加api接口

--- src/services/stephen-backend/index.ts
+++ src/services/stephen-backend/index.ts
@@ -3,8 +3,12 @@
 // API 更新时间：
 // API 唯一标识：
 import * as wxMpController from './wxMpController';
+import * as authController from './authController';
+import * as captchaController from './captchaController';
+import * as conversionController from './conversionController';
 import * as searchController from './searchController';
 import * as fileController from './fileController';
+import * as messageController from './messageController';
 import * as postController from './postController';
 import * as postFavourController from './postFavourController';
 import * as postThumbController from './postThumbController';
@@ -12,8 +16,12 @@ import * as tagController from './tagController';
 import * as userController from './userController';
 export default {
   wxMpController,
+  authController,
+  captchaController,
+  conversionController,
   searchController,
   fileController,
+  messageController,
   postController,
   postFavourController,
   postThumbController,
